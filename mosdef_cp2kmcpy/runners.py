235badf1959bfe78f7cff041d8f120d221e7471e
ramanishsingh <rsthakur922@gmail.com>
2021-05-18 11:37:24 -0500

    doc files

--- mosdef_cp2kmcpy/runners.py
+++ mosdef_cp2kmcpy/runners.py
@@ -7,7 +7,7 @@ def run_single_molecule_optimization(input_filename,output_filename):
 
 
 
-def run_md(input_filename,output_filename):
-    call("cp2k.popt -i {} -o {}".format(input_filename,output_filename),shell=True)
+def run_mc(input_filename,output_filename):
     print('Input file name given to runner is {}'.format(input_filename))
     print('Output file name  is {}'.format(output_filename))
+    call("cp2k.popt -i {} -i {} -o {}".format(input_filename[0],input_filename[1],output_filename),shell=True)
